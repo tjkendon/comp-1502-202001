d9871f48483ab8931285068197f991f065a3e64c
Tyson Kendon <tj@kendon.ca>
2020-01-16 11:15:33 -0700

    Added constructor logic

--- src/squash/Match.java
+++ src/squash/Match.java
@@ -11,7 +11,17 @@ public class Match {
 	
 	public Match(int gameCount) {
 		games = new ArrayList<>();
-		// add gameCount new games to the arrayList
+		if ((gameCount >= 1) && 
+				(gameCount <= 5) && 
+				(gameCount % 2 == 1)) {
+			for (int i = 0; i < gameCount; i++) {
+				games.add(new Game());
+			}
+			System.out.println("New match with " 
+					+ games.size() + " games");
+		} else {
+			System.err.println("Invalid # of games");
+		}
 	}
 	
 	// find the active game
@@ -23,10 +33,12 @@ public class Match {
 	}
 	
 	public void recordPlayerOnePoint() {
+		System.out.println("Player 1 Scores");
 		// find the active game & save the point
 	}
 	
 	public void recordPlayerTwoPoint() {
+		System.out.println("Player 2 Scores");
 		// find the active game & save the point
 	}
 	
