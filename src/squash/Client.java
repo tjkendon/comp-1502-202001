6dca43a5d44b6abf56942b3680de57a9371ce67b
Tyson Kendon <tj@kendon.ca>
2020-01-16 10:41:10 -0700

    Added loop

--- src/squash/Client.java
+++ src/squash/Client.java
@@ -7,6 +7,14 @@ public class Client {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
+		System.out.println("Squash:");
+		String line = in.next();
+		while (!line.equalsIgnoreCase("done")) {
+			
+			
+			line = in.next();
+		}
+		
 	}
 
 }
