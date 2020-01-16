8ef9a2df081e2d9767c5b0bc21ce2116dcf60b18
Tyson Kendon <tj@kendon.ca>
2020-01-16 11:10:45 -0700

    Switched to using match stubs

--- src/squash/Client.java
+++ src/squash/Client.java
@@ -10,28 +10,32 @@ public class Client {
 		System.out.println("Enter # of games:");
 		// create new match with # of games
 
+		int gameCount = in.nextInt();
+		Match match = new Match(gameCount);
+		
 		// match finds correct game to work on
+		
 		String line = in.next();
-		line = in.next();
 		while (!line.equalsIgnoreCase("done")) {
 			switch (line) {
 			case ("1"): // match records point for player 1
-				System.out.println("Player 1 scores");
+				match.recordPlayerOnePoint();
 				break;
 			case ("2"): // match records point for player 2
-				System.out.println("Player 2 scores");
+				match.recordPlayerTwoPoint();
 				break;
 			default:
 				System.out.println("Unrecognized input");
 			}
 			// output score
-			System.out.println("Current score is unknown");
+			System.out.println(match.getScore());
 			// check for the game winner
 			// check for the match winner
 			// if winner is found stop
 
 			line = in.next();
 		}
+		in.close();
 
 	}
 
