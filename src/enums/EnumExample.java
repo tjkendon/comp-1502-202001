b1ab705ebea62a6203d49a568fd631392a6647a1
Tyson Kendon <tj@kendon.ca>
2020-01-23 10:06:54 -0700

    Added example enum

---
+++ src/enums/EnumExample.java
@@ -0,0 +1,15 @@
+package enums;
+
+
+public class EnumExample {
+
+	// enum keyword, Name (Class Rules) 
+	// in braces, the possible values
+	// ends with a semicolon
+	enum Colour {RED, BLUE, GREEN};
+
+	public static void main(String args[]) {
+		
+	}
+	
+}
