79ec55acfa5b344fc8d2b0e6c169292de898e25d
Tyson Kendon <tj@kendon.ca>
2020-01-24 12:06:15 -0700

    Added score method declaration

--- src/squash/Game.java
+++ src/squash/Game.java
@@ -2,4 +2,21 @@ package squash;
 
 public class Game {
 
+	public final static int POINTS_PER_GAME = 11;
+	
+	private int playerOneScore = 0;
+	private int playerTwoScore = 0;
+	
+	
+	public Game() {
+		this.playerOneScore = 0;
+		this.playerTwoScore = 0;
+	}
+	
+	public void printP1Score() {
+		System.out.println(playerOneScore);
+	}
+	
+	
+	
 }
