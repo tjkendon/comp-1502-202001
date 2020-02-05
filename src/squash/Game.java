9b5cff4e54e862fa2efa46d7c0bacdcbaa085a87
Tyson Kendon <tj@kendon.ca>
2020-02-05 12:28:35 -0700

    # WARNING: head commit changed in the meantime
    Updated

--- src/squash/Game.java
+++ src/squash/Game.java
@@ -3,35 +3,38 @@ package squash;
 public class Game {
 
 	public final static int POINTS_PER_GAME = 11;
-	
+
 	private int playerOneScore = 0;
 	private int playerTwoScore = 0;
-	
-	
+
 	public Game() {
 		this.playerOneScore = 0;
 		this.playerTwoScore = 0;
 	}
-	
+
 	public void score(int player) {
-		switch(player) {
-		case 1: playerOneScore++; break;
-		case 2: playerTwoScore++; break;
+		if (!this.isWon()) {
+			switch (player) {
+			case 1:
+				playerOneScore++;
+				break;
+			case 2:
+				playerTwoScore++;
+				break;
+			}
 		}
 	}
-	
+
 	public boolean isWon() {
-		if (((playerOneScore >= 11) && 
-		   (playerOneScore >= playerTwoScore +2)) || 
-				((playerTwoScore >= 11) && 
-						   (playerTwoScore >= playerOneScore +2))) {
+		if (((playerOneScore >= 11) && (playerOneScore >= playerTwoScore + 2))
+				|| ((playerTwoScore >= 11) && (playerTwoScore >= playerOneScore + 2))) {
 			return true;
 		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 	public String scoreString() {
 		return playerOneScore + " - " + playerTwoScore;
 	}
@@ -43,5 +46,5 @@ public class Game {
 	public int getPlayerTwoScore() {
 		return playerTwoScore;
 	}
-	
+
 }
