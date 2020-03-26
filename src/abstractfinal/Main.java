75bd06c5b21eb0f48c808837585c66906cf61ed2
Tyson Kendon <tj@kendon.ca>
2020-03-26 10:09:36 -0600

    Commented enough out so that the class project actually compiles without complaint again.

---
+++ src/abstractfinal/Main.java
@@ -0,0 +1,16 @@
+package abstractfinal;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		SubExample x = new SubExample();
+//		x.doStuff();
+		
+		//Example y = new Example();
+		
+		
+	}
+	
+	
+	
+}
---
+++ src/abstractfinal/SubExample.java
@@ -0,0 +1,17 @@
+package abstractfinal;
+////
+////public  class SubExample extends Example{
+////
+////	@Override
+////	public void doAbstractStuff() {
+////		// TODO Auto-generated method stub
+////		
+////	}
+////	
+////	public void doFinalStuff() {
+////		System.out.println("I'm doing stuff!");
+////	}
+////	
+//	
+//
+//}
